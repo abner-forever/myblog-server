4d90047334714f7b6ec3aee3cea00c5d9b5a3580
Abner <1661287843@qq.com>
2021-11-18 20:22:42 +0800

    feat: 更改部署方式

--- build.sh
+++ build.sh
@@ -22,18 +22,9 @@ if [ $? = '0' ];then
 echo "解压成功～"
 else 
 echo "解压失败！"
-fi
-
-echo 上传
-scp -r ./dist root@139.155.232.209:/www/node    # 传入文件夹 -r 
-
-if [ $? = '0' ];then
-echo "上传成功～"
-else 
-echo "上传失败！"
+exit
 fi
 echo 清除缓存。。。
 rm -f *.tar.gz
 rm -rf ./dist
 echo 缓存清楚成功，退出脚本～
-exit
